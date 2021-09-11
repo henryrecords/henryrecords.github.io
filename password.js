9d69a4848d3caeb553163655cca9e4bf5f4c2749
henryrecords <45928298+henryrecords@users.noreply.github.com>
2021-09-10 21:07:02 -0700

    Update password.js

--- password.js
+++ password.js
@@ -1,11 +1,9 @@
-
-      var password;
-      var pass1="wsy072303";
-      password=prompt('请输入密码','');
-      if (password==pass1)
-        alert('密码正确，请确认是否进入');
-      else
-      {
-        window.location="http://www.google.com";
-      }
-
+var password;
+var pass1="wsy072303";
+password=prompt('请输入密码','');
+if (password==pass1)
+alert('密码正确，请确认是否进入');
+else
+{
+window.location="http://www.google.com";
+}
