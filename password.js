3d1a70e898c570bbf68ae02f82d8475694212a1e
henryrecords <45928298+henryrecords@users.noreply.github.com>
2021-09-10 20:56:20 -0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,13 @@
+<script language="JavaScript">
+      <!--hide
+      var password;
+      var pass1="wsy072303";
+      password=prompt('请输入密码','');
+      if (password==pass1)
+        alert('密码正确，请确认是否进入');
+      else
+      {
+        window.location="http://www.google.com";
+      }
+      //-->
+    </script>
